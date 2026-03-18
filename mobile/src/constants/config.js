dff3e1cb80b5f4b2aa5f3e1d42922246e7ca1f99
Asif Shaik <asifshaik0528@gmail.com>
2026-03-18 12:43:07 +0530

    Update config and add base64.txt

--- mobile/src/constants/config.js
+++ mobile/src/constants/config.js
@@ -1,5 +1,5 @@
 // Replace with your machine's IP address if running on physical device
 // For Android Emulator, 10.0.2.2 points to localhost of the host machine.
 // For iOS Simulator, localhost works.
-// export const API_URL = 'https://art-bloom.onrender.com/api'; // Render Backend
-export const API_URL = 'http://10.1.173.148:5000/api'; // Local Backend
+export const API_URL = 'https://art-bloom.onrender.com/api'; // Render Backend
+//export const API_URL = 'http://10.1.173.148:5000/api'; // Local Backend
